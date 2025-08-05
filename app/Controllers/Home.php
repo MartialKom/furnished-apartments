60447348fd24dbddfd27e5cb053abb7a396f8c53
martial <martial.kom@abyster.com>
2025-08-05 10:50:02 +0100

    integrate main page

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -6,6 +6,11 @@ class Home extends BaseController
 {
     public function index(): string
     {
+        return view('home');
+    }
+    public function apartments(): string
+    {
         return view('welcome_message');
     }
+
 }
