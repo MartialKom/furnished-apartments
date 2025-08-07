833dad95cb9b4fa7d6f6f996d07eadb68a4a6ba5
martial <martial.kom@abyster.com>
2025-08-07 10:15:12 +0100

    Update routes an php file for Admin UI

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -5,6 +5,15 @@ use CodeIgniter\Router\RouteCollection;
 /**
  * @var RouteCollection $routes
  */
-$routes->get('/', 'Home::index');
 
-$routes->get('/apartments', 'Home::apartments');
+
+
+
+$routes->group('', ['namespace' => 'App\Controllers\Frontend'], function ($routes) {
+    $routes->get('/', 'Home::index');
+    $routes->get('/apartments', 'Home::apartments');
+});
+
+$routes->group('', ['namespace' => 'App\Controllers\Admin'], function ($routes) {
+    $routes->get('/', 'DashboardController::index');
+});
